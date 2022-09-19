7ec72725a7fa72c2f60ca96ed47f56dca5df5121
vestjoe <mrjoevest@gmail.com>
2022-09-19 07:42:58 -0500

    img fix

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -7,21 +7,21 @@ const FeatureList = [
   {
     title: 'Get a paperback copy',
     Png: require('../../static/img/book-cover.png').default,
-    Link: "https://www.amazon.com/dp/B083XVG633/ref=sr_1_2?keywords=red+team+development&qid=1579624713&sr=8-2&tag=cybersecur05a-20",
+    Link: "https://www.amazon.com/dp/B083XVG633/ref=sr_1_2?keywords=red+team+development&qid=1579624713&sr=8-2&tag=cybersecur094-20",
     description: (
       <>
-        Purchase a paperback copy from Amazon
+        Purchase a paperback copy
       </>
     ),
   },
   {
-    title: 'Get a copy on Kindle',
-    Png: require('../../static/img/kindle-cover.png').default,
-    Link: "https://www.amazon.com/gp/product/B0842BMMCC?pf_rd_p=ab873d20-a0ca-439b-ac45-cd78f07a84d8&pf_rd_r=2P19FT2TFRS8R4D4KP3V&tag=cybersecur05a-20",
+    title: 'Get a digital copy',
+    Png: require('../../static/img/digital-cover.png').default,
+    Link: "https://www.amazon.com/gp/product/B0842BMMCC?pf_rd_p=ab873d20-a0ca-439b-ac45-cd78f07a84d8&pf_rd_r=2P19FT2TFRS8R4D4KP3V&tag=cybersecur094-20",
 
     description: (
       <>
-        Purchase a kindle copy from Amazon
+        Purchase a ditigal copy
       </>
     ),
   },
